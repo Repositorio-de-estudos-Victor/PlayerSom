0fb5e1b603c7ec74aba3caf9a3caf446030d6128
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-05-11 20:55:18 -0300

    por player de som

--- Player Som/ViewController.swift
+++ Player Som/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
+    
+    @IBOutlet weak var sliderVolume: UISlider!
+    
+    @IBAction func atualizarVolume(_ sender: Any) {
+        player.volume = sliderVolume.value
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        player.play()
+    }
+    
+    @IBAction func pause(_ sender: Any) {
+        player.pause()
+        
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        player.stop()
+        player.currentTime = 0
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let path = Bundle.main.path(forResource: "J_Syreus_Bach_-_Lesser_Faith", ofType: "mp3") {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                player.prepareToPlay()
+                
+                
+            } catch {}
+            
+        }
+        
     }
 
 
